0b8629376eb9461fa6e3db227959049be15dbc75
Lvhac <819902996@qq.com>
2019-05-21 21:30:38 +0800

    添加测试文件

---
+++ src/main/java/com/test/Helloworld.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @author ChuanLv
+ * @version v1.0
+ * @date 2019/5/21 21:24
+ * @description TODO
+ **/
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+    }
+}
